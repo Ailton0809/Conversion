a630c94ea65d0f59097f578c8521851e3f92e424
191G0251 <davidailton0954@outlook.com>
2023-10-02 22:14:26 -0600

    Agregar archivos de proyecto.

---
+++ Conversion/Controllers/Home/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Conversion.Models.ViewModels.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Conversion.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(IndexViewModel vm)
+        {
+            if (vm == null)
+                return NotFound();
+           
+            if (vm.Valor == 0)
+                return View(vm);
+            
+            if (vm.Moneda == null)
+                return View(vm);
+            
+            if (vm.Moneda == "USD")
+            {vm.Resultado = vm.Valor / 20m;}
+
+            if (vm.Moneda == "MXN")
+            { vm.Resultado = vm.Valor * 20m;}
+            
+            return View(vm);
+        }
+    }
+}
---
+++ Conversion/Models/ViewModels/IndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Conversion.Models.ViewModels.ViewModels
+{
+    public class IndexViewModel
+    {
+        public string Moneda { get; set; }
+        public decimal Valor { get; set; }
+        public decimal Resultado { get; set; }
+    }
+}
